eb577bf6f0cb863235120b2e665b536bf41dd68a
Programmika <rothe.annika@yahoo.com>
2024-04-18 18:08:32 +0200

    navbar,logo & icon added

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import { FaCloudSun } from "react-icons/fa";
 
 type Props = {}
 
-export default function navbar({}: Props) {
+export default function navbar({ }: Props) {
     return (
-        <nav className="">Navbar</nav>
-    )
+        <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+            <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
+                <div className="flex items-center justify-center gap-2 ">
+                    <h2 className="text-gray-500 text-3xl">Weather</h2>
+                    <FaCloudSun className="text-3xl mt-1 text-yellow-500" />
+                </div>
+                {/* */}
+            </div>
+        </nav>
+    );
 }
