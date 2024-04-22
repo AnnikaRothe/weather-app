13a5b6aa91168c38d4d9f9c7da8a49d1919e8233
Programmika <rothe.annika@yahoo.com>
2024-04-22 14:46:12 +0200

    added searchbar and icons to the navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { FaCloudSun } from "react-icons/fa";
+import { MdMyLocation } from "react-icons/md";
+import { GrLocation } from "react-icons/gr";
+import Searchbox from "./searchbox";
 
 type Props = {}
 
-export default function navbar({ }: Props) {
+export default function Navbar({ }: Props) {
     return (
-        <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <nav className="shadow-sm px-20 sticky top-0 left-0 z-50 bg-white">
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
                 <div className="flex items-center justify-center gap-2 ">
                     <h2 className="text-gray-500 text-3xl">Weather</h2>
                     <FaCloudSun className="text-3xl mt-1 text-yellow-500" />
                 </div>
                 {/* */}
+                <section className="flex gap-2 items-center">
+                <MdMyLocation className="text-2xl text-gray-400 hover:opacity-70 cursor-pointer " />
+                <GrLocation className="text-2xl" />
+                <p className="text-slate-900/80 text-sm"> Scotland </p>
+                <div>{/* SearchBox */}
+                <Searchbox value={""} onChange={undefined} onSubmit={undefined} />
+                </div>
+                </section>
             </div>
         </nav>
     );
---
+++ src/app/components/searchbox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { IoMdSearch } from "react-icons/io";
+import { cn } from "../utils/cn";
+
+
+type Props = {
+className?:string
+value:string;
+onChange:React.ChangeEventHandler<HTMLInputElement> |
+undefined;
+onSubmit:React.FormEventHandler<HTMLFormElement> |
+undefined;
+
+
+
+}
+
+
+export default function Searchbox(props: Props) {
+    return (
+        <form onSubmit={props.onSubmit} 
+        className={cn("flex relative items-center justify-center h-10",props.className)}>
+            <input 
+            type="text"
+            value={props.value}
+        onChange={props.onChange}
+            placeholder="Search location.."
+            className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none
+             focus:border-green-600 h-full" 
+             />
+            <button className="px-4 py-[9px] bg-green-600 text-white rounded-r-md 
+            focus:outline-none hover:bg-green-500 h-full">
+            <IoMdSearch />
+            </button>
+        </form>
+    );
+}
+   
