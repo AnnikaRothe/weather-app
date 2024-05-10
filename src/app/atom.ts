c16c814ecd003335aead671eee62e34efbfece9a
Programmika <rothe.annika@yahoo.com>
2024-05-10 11:41:49 +0200

    added current loc.&responsivness

---
+++ src/app/atom.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+export const placeAtom = atom("Inverness");
+
+export const loadingCityAtom = atom(false);
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,17 +6,21 @@ import { MdMyLocation } from "react-icons/md";
 import { GrLocation } from "react-icons/gr";
 import Searchbox from "./searchbox";
 import axios from "axios";
+import { useAtom } from "jotai";
+import { loadingCityAtom, placeAtom } from "../atom";
 
-type Props = {};
+type Props = { location?: string };
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
-export default function Navbar({}: Props) {
+export default function Navbar({ location }: Props) {
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
   //
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [place, setPlace] = useAtom(placeAtom);
+  const [_, setLoadingCity] = useAtom(loadingCityAtom);
 
   async function handleInputChange(value: string) {
     setCity(value);
@@ -44,46 +48,96 @@ export default function Navbar({}: Props) {
     setShowSuggestions(false);
   }
 
-  function handleSubmitSearch(e:React.FormEvent<HTMLFormElement>) {
-
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    setLoadingCity(true);
     e.preventDefault();
-    if(suggestions.length == 0){
-        setError("Location not found");
-    }
-    else{
-        setError('');
+    if (suggestions.length == 0) {
+      setError("Location not found");
+      setLoadingCity(false);
+    } else {
+      setError("");
+      setTimeout(() => {
+        setLoadingCity(false);
+        setPlace(city);
         setShowSuggestions(false);
+      }, 500);
+    }
+  }
+
+  function handleCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          }, 500);
+        } catch (error) {
+          setLoadingCity(false);
+        }
+      });
     }
   }
 
   return (
-    <nav className="shadow-sm px-20 sticky top-0 left-0 z-50 bg-white">
-      <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
-        <div className="flex items-center justify-center gap-2 ">
-          <h2 className="text-gray-500 text-3xl">Weather</h2>
-          <FaCloudSun className="text-3xl mt-1 text-yellow-500" />
-        </div>
-        <section className="flex gap-2 items-center">
-          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-70 cursor-pointer " />
-          <GrLocation className="text-2xl" />
-          <p className="text-slate-900/80 text-sm"> Scotland </p>
-          <div className="relative">
-            {/* SearchBox */}
-            <Searchbox
-              value={"city"}
-              onSubmit={handleSubmitSearch}
-              onChange={(e) => handleInputChange(e.target.value)}
-            />
-            <SuggestionBox
-           {...{showSuggestions,
-            suggestions,
-            handleSuggestionClick,
-            error}}
-            />
+    <>
+      <nav className="shadow-sm px-20 sticky top-0 left-0 z-50 bg-white">
+        <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
+          <div className="flex items-center justify-center gap-2 ">
+            <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <FaCloudSun className="text-3xl mt-1 text-yellow-500" />
           </div>
-        </section>
-      </div>
-    </nav>
+          <section className="flex gap-2 items-center">
+            <MdMyLocation
+              title="Your current location"
+              onClick={handleCurrentLocation}
+              className="text-2xl text-gray-400 hover:opacity-70 cursor-pointer "
+            />
+            <GrLocation className="text-2xl" />
+            <p className="text-slate-900/80 text-sm"> {location} </p>
+            <div className="relative hidden md:flex">
+              {/* SearchBox */}
+              <Searchbox
+                value={"city"}
+                onSubmit={handleSubmitSearch}
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+              <SuggestionBox
+                {...{
+                  showSuggestions,
+                  suggestions,
+                  handleSuggestionClick,
+                  error,
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className="flex max-w-7xl px-3 md:hidden">
+        <div className="relative">
+          {/* SearchBox */}
+          <Searchbox
+            value={"city"}
+            onSubmit={handleSubmitSearch}
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+          <SuggestionBox
+            {...{
+              showSuggestions,
+              suggestions,
+              handleSuggestionClick,
+              error,
+            }}
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
